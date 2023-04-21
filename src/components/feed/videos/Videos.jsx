92a4e453da60580dbc7c280765082a72061205aa
Tr0ngTin <71576097+tr0ngtin11@users.noreply.github.com>
2023-04-21 18:04:38 +0700

    6:04 21/4

--- src/components/feed/videos/Videos.jsx
+++ src/components/feed/videos/Videos.jsx
@@ -1,158 +1,73 @@
 import { Box, Grid, Skeleton } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import VideoCard from "./VideoCard";
 import { useSelector } from "react-redux";
-import SampleCard from "./SampleCard";
-const Videos = ({ videos, pageNumber, loaderBottom, loaderTop }) => {
+const Videos = ({ videos, loaderBottom, loaderTop, pageNumber }) => {
   const { isLoading, data } = useSelector((state) => state.videos);
   const renderVideo = () => {
     console.log("videos", videos);
     console.log("dataLENGTH", data.length);
     console.log("page", pageNumber);
-    let NewList = data.map((ob, index) =>
-      ob.items.map((video, index) => {
-        if (index === 0) {
-          if (video === undefined) {
-            return (
-              <>
-                <SampleCard index={index} />
-              </>
-            );
-          } else {
-            return (
-              <Grid
+
+    var NewList = data.map((ob, index) => {
+      return ob.items.map((video, index) => {
+        if (video === undefined) {
+          return (
+            <Grid
+              sx={{
+                paddingTop: "0px",
+              }}
+              item
+              xs={12}
+              sm={12}
+              md={4}
+              lg={4}
+              xl={3}
+              key={index}
+            >
+              <Skeleton
                 sx={{
-                  paddingTop: "0px",
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                    lg: "block",
+                    xl: "block",
+                  },
+                  mb: "20px",
                 }}
-                item
-                xs={12}
-                sm={12}
-                md={4}
-                lg={4}
-                xl={3}
-                key={video.id.concat(index)}
-              >
-                <VideoCard
-                  key={video.id}
-                  video={video}
-                  idVideo={video.snippet.resourceId.videoId}
-                />
-              </Grid>
-            );
-          }
-        } else if (index === ob.items.length - 1) {
-          if (video === undefined) {
-            return (
-              <Grid
-                sx={{
-                  paddingTop: "0px",
-                }}
-                item
-                xs={12}
-                sm={12}
-                md={4}
-                lg={4}
-                xl={3}
-                key={index}
-              >
-                <Skeleton
-                  sx={{
-                    display: {
-                      xs: "none",
-                      sm: "none",
-                      md: "block",
-                      lg: "block",
-                      xl: "block",
-                    },
-                    mb: "20px",
-                  }}
-                  animation="wave"
-                  variant="rect"
-                  // width="30%"
-                  height={250}
-                />
-              </Grid>
-            );
-          } else {
-            return (
-              <Grid
-                sx={{
-                  paddingTop: "0px",
-                }}
-                item
-                xs={12}
-                sm={12}
-                md={4}
-                lg={4}
-                xl={3}
-                key={video.id.concat(index)}
-              >
-                <VideoCard
-                  key={video.id}
-                  video={video}
-                  idVideo={video.snippet.resourceId.videoId}
-                />
-              </Grid>
-            );
-          }
+                animation="wave"
+                variant="rect"
+                // width="30%"
+                height={250}
+              />
+            </Grid>
+          );
         } else {
-          if (video === undefined) {
-            return (
-              <Grid
-                sx={{
-                  paddingTop: "0px",
-                }}
-                item
-                xs={12}
-                sm={12}
-                md={4}
-                lg={4}
-                xl={3}
-                key={index}
-              >
-                <Skeleton
-                  sx={{
-                    display: {
-                      xs: "none",
-                      sm: "none",
-                      md: "block",
-                      lg: "block",
-                      xl: "block",
-                    },
-                    mb: "20px",
-                  }}
-                  animation="wave"
-                  variant="rect"
-                  // width="30%"
-                  height={250}
-                />
-              </Grid>
-            );
-          } else {
-            return (
-              <Grid
-                sx={{
-                  paddingTop: "0px",
-                }}
-                item
-                xs={12}
-                sm={12}
-                md={4}
-                lg={4}
-                xl={3}
-                key={video.id.concat(index)}
-              >
-                <VideoCard
-                  key={video.id}
-                  video={video}
-                  idVideo={video.snippet.resourceId.videoId}
-                />
-              </Grid>
-            );
-          }
+          return (
+            <Grid
+              sx={{
+                paddingTop: "0px",
+              }}
+              item
+              xs={12}
+              sm={12}
+              md={4}
+              lg={4}
+              xl={3}
+              key={video.id.concat(index)}
+            >
+              
+              <VideoCard
+                key={video.id}
+                video={video}
+                idVideo={video.snippet.resourceId.videoId}
+              />
+            </Grid>
+          );
         }
-      })
-    );
+      });
+    });
 
     return NewList;
   };
